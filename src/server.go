0126eef575907fcc33c23289366caa8bdb6bf3ed
Ritesh Singh <riteshsingh@microsoft.com>
2022-01-24 09:53:19 +0530

    intial implementation of trie -> https://github.com/Shubham8287/flash/issues/1

--- src/server.go
+++ src/server.go
@@ -4,8 +4,11 @@ import (
     "fmt"
     "html"
     "log"
-    "net/http"
+    "net/http",
+    "trie"
 )
+
+trie := initTrie()
 
 func main() {
 
@@ -16,6 +19,15 @@ func main() {
     http.HandleFunc("/isAlive", func(w http.ResponseWriter, r *http.Request){
         fmt.Fprintf(w, "Boss is always fine")
     })
+
+    http.HandleFun("/find", func(w http.ResponseWriter, r *http.Request)) {
+        prefix = r.FormValue("prefix")
+        if(trie.find(prefix)) {
+            fmt.Fprintf(w, "Found the prefix")
+        } else {
+            fmt.Fprintf(w, "Not found any matching")
+        }
+    }
 
     log.Fatal(http.ListenAndServe(":8080", nil))
 
