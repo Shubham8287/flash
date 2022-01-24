1c8db579570c07bdb16d45f552ba7dcdbf5a01bd
Shubham Mishra <shivam828787@gmail.com>
2022-01-24 21:03:53 +0530

    loaded intial data

--- src/server.go
+++ src/server.go
@@ -5,10 +5,33 @@ import (
     "html"
     "log"
     "net/http"
+    "encoding/json"
+    "io/ioutil"
 )
+
+const DataPath = "./data/words_dictionary.json";
 
 func main() {
     trie := initTrie();
+
+    fmt.Println("reading data...")  
+    fileData, err := ioutil.ReadFile(DataPath);
+    if err != nil {
+        fmt.Print(err)
+    }
+    
+    var intialWordSet = []string{}
+    fmt.Println("Unmarshalling data...")
+    err = json.Unmarshal(fileData, &intialWordSet)
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+
+    for i := 0; i < len(intialWordSet); i++ {
+        trie.insert(intialWordSet[i]);
+    }
+
+    fmt.Println("Server Started...")
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
