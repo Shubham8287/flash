409bf83dfad13169e3c15f54066be3a154c33f86
RiteshKSingh1729 <ritesh.kumar465@gmail.com>
2022-02-19 16:23:31 +0530

    one more time, removed unnecessary files

--- client/typeahead-search-app/src/index.js
+++ client/typeahead-search-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import NavbarTop from './components/Navbar';
