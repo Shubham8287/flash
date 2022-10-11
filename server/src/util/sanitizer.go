40b5821bd5709c4f47ca209f269baa5829da66e2
Ritesh Singh <ritesh.kumar465@gmail.com>
2022-10-11 22:10:17 +0530

    sanitize incoming request

---
+++ server/src/util/sanitizer.go
@@ -0,0 +1,32 @@
+package util
+import (
+	"fmt"
+	"strings"
+	"regexp"
+)
+
+// Sanitized the input prefix recieved.
+func SanitizePrefix(prefix string) string {
+	// regex expression
+	nonSpecialCharacterRegex := regexp.MustCompile(`[^a-zA-Z/-]+`)
+	
+	var  sanitizedPrefix  string = "";
+
+	// trim the string
+	sanitizedPrefix = strings.TrimSpace(prefix)
+
+	// add "-" for in -between spaces
+	sanitizedPrefix = strings.ReplaceAll(sanitizedPrefix, " ", "-")
+
+	// remove special characters
+	sanitizedPrefix = nonSpecialCharacterRegex.ReplaceAllString(sanitizedPrefix, "")
+
+	// lowercase all the upercase letters
+	sanitizedPrefix = strings.ToLower(sanitizedPrefix)
+
+	// print the sanitized string
+	fmt.Println(sanitizedPrefix)
+
+	// return the sanitized string
+	return sanitizedPrefix
+}
